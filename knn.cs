1a8c4476b86e9125248d345b426af106c9d7da8b
winused <119756316+winused@users.noreply.github.com>
2022-12-03 22:33:51 +0300

    Add files via upload

---
+++ knn.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DSProje_1
+{
+
+    class Program
+    {
+        private static double[,] CreateMatrix(double width, double height, int n)
+        {
+
+
+            double[,] matrix = new double[n, 2];
+
+            Random random = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i, 0] = (random.NextDouble()) * width;
+                matrix[i, 1] = (random.NextDouble()) * height;
+            }
+
+            Console.WriteLine($"\n\n{width}x{height} Öklid uzayındaki rastgele noktalardan oluşan {n}x2 matris :\n");
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                    Console.Write(String.Format("{0:0.0}", matrix[i, j]) + "\t");
+                Console.WriteLine("\n");
+            }
+
+            return matrix;
+        }
+
+        private static void YazdırCreateDistanceMatrix(double[,] distance_matrix)
+        {
+
+            Console.WriteLine($"\n\n{distance_matrix.GetLength(0)}x{distance_matrix.GetLength(1)} uzaklık matrisi :\n");
+
+            for (int i = 0; i < distance_matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < distance_matrix.GetLength(1); j++)
+                    Console.Write(String.Format("{0:0.0}", distance_matrix[i, j]).Replace(",", ".") + "\t");
+
+                Console.WriteLine("\n");
+            }
+        }
+        private static double[,] CreateDistanceMatrix(double[,] matrix)
+        {
+            double[,] distance_matrix = new double[matrix.GetLength(0), matrix.GetLength(0)];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    distance_matrix[i, j] = Math.Sqrt(Math.Pow(matrix[i, 0] - matrix[j, 0], 2) + Math.Pow(matrix[i, 1] - matrix[j, 1], 2));
+                }
+            }
+
+            YazdırCreateDistanceMatrix(distance_matrix);
+
+            return distance_matrix;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen Öklid uzayının genişliğini giriniz : ");
+            double width = double.Parse(Console.ReadLine());
+
+            Console.Write("Lütfen Öklid uzayının yüksekliğini giriniz : ");
+            double height = double.Parse(Console.ReadLine());
+
+            Console.Write("Lütfen kaç adet nokta oluşturulacağını belirtiniz : ");
+            int number_of_dots = int.Parse(Console.ReadLine());
+            double[,] matrix = CreateMatrix(width, height, number_of_dots);
+            double[,] distance_matrix = CreateDistanceMatrix(matrix);
+            knn(width, height, number_of_dots, distance_matrix);
+            Console.ReadKey();
+        }
+
+        static void knn(double width, double height, int n, double[,] distance_matrix)
+        {
+            Random random = new Random();
+            Console.Write("Kaç tur döndürüleceğini belirtiniz : ");
+            int tur_count = int.Parse(Console.ReadLine());
+            List<int> ilk_noktalar = new List<int>();
+            int nokta;
+            YazdırCreateDistanceMatrix(distance_matrix);
+            for (int s = 0; s < tur_count; s++)
+            {
+                do
+                {
+                    nokta = random.Next(0, tur_count);
+
+                } while (ilk_noktalar.Contains(nokta));
+
+                List<int> kullanilmayacaklar = new List<int>();
+               
+                ilk_noktalar.Add(nokta);
+ 
+
+                double min_uzaklık;
+                int min_uzak_nok;
+                double toplam_uzaklık = 0;
+
+                for (int j = 0; j < n; j++)
+                {
+
+                    kullanilmayacaklar.Add(nokta);
+                    min_uzaklık = width * height;
+                    min_uzak_nok = -1;
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        
+                        if (!kullanilmayacaklar.Contains(i))
+                        {
+                            double arasi_uzaklik;
+                            arasi_uzaklik = distance_matrix[nokta, i];
+                            if (arasi_uzaklik < min_uzaklık)
+                            {
+                                min_uzaklık = arasi_uzaklik;
+                                min_uzak_nok = i;
+                            }
+
+                        }
+                    }
+                    //Console.WriteLine("toplama katılan min uzaklık" + min_uzaklık+" toplanan nokta="+min_uzak_nok);
+                    
+                    if (min_uzak_nok != -1)
+                    {
+                        toplam_uzaklık += min_uzaklık;
+                        nokta = min_uzak_nok;
+                    }
+                }
+
+                Console.WriteLine("\nTur numarası: " + (s + 1));
+                for (int i = 0; i < kullanilmayacaklar.Count - 1; i++)
+                {
+                    Console.Write(kullanilmayacaklar[i] + "-");
+
+                }
+                Console.Write(kullanilmayacaklar[kullanilmayacaklar.Count - 1]);
+
+
+                Console.WriteLine("\nTurun toplam uzunluğu: " + toplam_uzaklık);
+                kullanilmayacaklar.Clear();
+
+            }
+        }
+    }
+}
---
+++ perceptron.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace Perceptron
+{
+    public class Neuron
+    {
+
+        private static readonly Random random = new Random();
+
+        private static double RandomNumberBetween(double minimum, double maximum)
+        {
+
+            Random random = new Random();
+            return random.NextDouble() * (maximum - minimum) + minimum;
+
+        }
+
+
+        // -1 ve 1 arası random ağırlıklar
+        public double[] weights = { RandomNumberBetween(-1,1),RandomNumberBetween(-1,1) };
+
+        // Training data
+        public double[,] inputs = { { 6, 5 }, { 2, 4 }, { -3, -5 }, { -1, -1 }, { 1, 1 }, { -2, 7 }, { -4, -2 }, { -6, 3 } };
+        
+        //Target
+        public int[] outputs = new int[] { 1, 1, -1, -1, 1, 1, -1, -1 }; 
+
+    }
+
+    public class Eğitim
+
+    {
+
+        static void Main()
+        {
+
+
+            Neuron neuron = new Neuron();
+
+            double delta = 0.05;
+
+            int epok = 1;
+
+            int yanlis ;
+            int Count = neuron.inputs.GetUpperBound(0) + 1;
+
+
+            Console.Write("Lütfen kaç epok dönüleceğini giriniz : ");
+            int epok_sayisi = int.Parse(Console.ReadLine());
+
+             for (int s = 0; s < epok_sayisi; s++)
+             {
+ 
+                yanlis = 0;
+                for (int j = 0; j < Count; j++)
+                {
+                    // Toplama işlemi yapılır ve pozitif veya negatifliğine göre 1 veya -1 döndürülür.
+                    int output = Output(neuron.weights, neuron.inputs[j, 0], neuron.inputs[j, 1]);
+                    // Hata yok ise t-o = 0 çıkar. neuron.outputs[j] bize t'yi verir.
+                    double hata_payi = neuron.outputs[j] - output;
+
+                    if (hata_payi != 0)
+                    {
+                        yanlis++;
+                        // x1 ve x2 ağırlıkları değişir
+                        //Projede x1/10, x2/10 değeri ile ağırlıkların değişmesi istendiği için 10a bölünür.
+                        neuron.weights[0] += delta * hata_payi * neuron.inputs[j, 0] / 10;
+                        neuron.weights[1] += delta * hata_payi * neuron.inputs[j, 1] / 10;
+                     
+                    }
+         
+                }
+                int dogru = Count - yanlis;
+                double dogruluk = (double)dogru / (double)Count;
+                Console.WriteLine("Epok {0}\tDoğru{1}\tAccuracy %{2}", epok, dogru, dogruluk * 100);
+                epok++;
+
+             }
+
+            //////Eğitimimin sonuna gelindi. w1 ve w2 değerlerimiz eğitildi.
+
+
+            Console.Write("Konsola girilecek test veri çifti sayısı giriniz:");
+            int veri_say = int.Parse(Console.ReadLine());
+
+            int dogru_cikti = 0;
+            int[,] veriler = new int[veri_say, 2];
+            int[] test_target = new int[veri_say];
+            int[] ciktilar = new int[veri_say];
+            for (int m = 0; m < veri_say; m++)
+            {
+                //Kolsoldan her veri çifti için x1 ve x2 değerleri alınır ve 2 boyutlu arrayde saklanır.
+                Console.WriteLine("{0}. veri için", m + 1);
+                Console.Write("X1 = ");
+                veriler[m,0] = int.Parse(Console.ReadLine());
+                Console.Write("X2 = ");
+                veriler[m, 1] = int.Parse(Console.ReadLine());
+                test_target[m] = ((veriler[m,0]+ veriler[m, 1]) >= 0) ? 1 : -1;
+                ciktilar[m] = Output(neuron.weights, veriler[m, 0], veriler[m, 1]);
+
+                if(test_target[m] == ciktilar[m])
+                {
+                    dogru_cikti++;
+                }
+            }
+
+
+            double test_dogruluk = (double)dogru_cikti / (double)veri_say;
+            Console.WriteLine("Doğru{0}\tAccuracy %{1}", dogru_cikti, test_dogruluk * 100);
+
+
+            Console.ReadKey();
+        }
+
+        private static int Output(double[] weights, double x1, double x2)
+        {
+            //  Output = x1 * w1 + x2 * w2 
+            double islem = x1 * weights[0] + x2 * weights[1];
+            // İşlemin sonucu 0dan büyükse output = 1 küçükse output = -1 döndürür.
+            return (islem >= 0) ? 1 : -1;
+        }
+
+
+    }
+}
